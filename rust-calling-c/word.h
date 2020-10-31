8b26eac15b6150acbfbabc8ef4a0e34326bb4c9c
adwait1-g <adwait.gautham@gmail.com>
2020-11-01 01:35:11 +0530

    Updated rust-calling-c

--- rust-calling-c/word.h
+++ rust-calling-c/word.h
@@ -5,14 +5,14 @@
 
 typedef struct word_info
 {
-    char *word;
+    const char *word;
     bool validity;
 } word_info_t;
 
 
-char* get_word(word_info_t *w_info);
+const char* get_word(const word_info_t *w_info);
 
-bool get_validity(word_info_t *w_info);
+bool get_validity(const word_info_t *w_info);
 
 
 #endif /* __WORD_H__ */
